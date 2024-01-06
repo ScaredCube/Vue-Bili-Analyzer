f5dea3b2c2e9cff678159acca48af954e52d2cdc
ScaredCube <74418739+ScaredCube@users.noreply.github.com>
2024-01-07 01:39:24 +0800

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="container mx-auto mt-5">
     <h1 class="text-3xl font-bold mb-4">A Bili-video Analyzer & Downloader</h1>
+
+    <p class="text-gray-500">稳定下载为固定下载链接 速度较慢 不会失效 使用<a href="https://sccube.link/api" target="_blank">自建api</a>实现</p>
+    <p class="text-gray-500">高速下载链接为B站官方CDN节点(阿里云) 速度较快 会失效 请勿长时间下载</p>
+    <br>
     <p class="text-gray-500">Made with 
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue</a>
        + <a href="https://www.typescriptlang.org/" target="_blank" rel="noopener">TypeScript</a>
