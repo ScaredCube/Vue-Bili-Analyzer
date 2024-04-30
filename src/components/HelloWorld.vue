cbff7a242b6620366bcbc6320aab3063f1ad3b35
ScaredCube <74418739+ScaredCube@users.noreply.github.com>
2024-05-01 00:34:25 +0800

    add bulletin

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,8 +7,13 @@ defineProps<{
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
+    
     <h3>
     By <a href="https://sccube.link/" target="_blank" rel="noopener">ScaredCube</a>
+    </h3>
+    <!--<br>-->
+    <h3 class="text-slate-300">
+      *该服务目前暂时无法使用！详情请<a href="https://sccube.link/biliApi_securityControl" target="_blank">见此</a>
     </h3>
     
   </div>
